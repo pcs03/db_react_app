6e29495ebe65f6250f3a3f9bc4c6f477728e0358
Paolo Stet <paolostet@gmail.com>
2023-03-15 06:32:47 +0100

    Add components to app

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import mock_database from '../../data/mock_database.json'
+import UserList from '../UserList/userList'
+import InputField from './../../Components/InputField/InputField'
+
 
 function App () {
   return (
     <div className='app'>
-
+      <InputField />
+      <UserList 
+      users={mock_database}/>
     </div>
   )
 }
---
+++ src/Components/InputField/InputField.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function InputField () {
+  return <h1>Hi!</h1>
+}
+
+export default InputField;
